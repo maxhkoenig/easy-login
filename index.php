8a2abf3893f95c13defb5ddf58a90fcb3d821a6a
Maxkoe <maxkoenig@my-macbook-air.cg.shawcable.net>
2014-06-01 10:25:27 -0600

    Added Base Files
    Doesn’t do much yet, just need to have it there

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    include ("words/header/header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="description" content="Would you like to know how to describe anything? Click here to learn more!">
+        <title></title>
+         <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    </head>
+    <body>
+      
+    </body>
+</html>
+<?php
+    include ("words/footer/footer.php");
+?>
